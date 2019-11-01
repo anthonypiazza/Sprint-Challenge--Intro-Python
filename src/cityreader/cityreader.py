1a724154c253b4d7458a64af727f83bb7b8c49e6
anthonypiazza <piazza.anthony.n@gmail.com>
2019-11-01 11:02:46 -0500

    Completeded csv reader and creating class instances, MVP for sprint is met

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,6 +1,16 @@
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
 
+import csv
+
+class City:
+  def __init__(self, name, lat, lon):
+    self.name = name
+    self.lat = lat
+    self.lon = lon
+  
+  def __str__(self):
+    return f"City: {self.name}, Lat: {self.lat}, Lon: {self.lon}"
 
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
@@ -16,12 +26,18 @@
 # should not be loaded into a City object.
 cities = []
 
+
 def cityreader(cities=[]):
   # TODO Implement the functionality to read from the 'cities.csv' file
   # For each city record, create a new City instance and add it to the 
   # `cities` list
+  with open('cities.csv') as csvfile:
+    citiesreader = csv.reader(csvfile)
+    for c in citiesreader:
+      if c[0] != 'city':
+        cities.append(City(c[0], c[3], c[4]))
     
-    return cities
+  return cities
 
 cityreader(cities)
 
